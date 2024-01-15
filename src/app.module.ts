209bf1281ac1cef26e684ebc4b7697085e2ad4ac
romelgomez <bmxandcode@gmail.com>
2024-01-15 13:59:38 -0500

    chore: add auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AppConfigModule, PrismaModule],
+  imports: [AppConfigModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
