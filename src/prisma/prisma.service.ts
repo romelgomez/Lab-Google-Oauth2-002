1701db24d4a8395e4fc96af887edb602b6012ed9
romelgomez <bmxandcode@gmail.com>
2024-01-15 13:01:37 -0500

    chore: setting the config module

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { AppConfigService } from 'src/config/config.service';
+import { ProcessEnvEnum } from 'src/config/config.types';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(protected appConfigService: AppConfigService) {
+    const host = appConfigService.getEnvVal(ProcessEnvEnum.DATABASE_HOST);
+    const port = appConfigService.getEnvVal(ProcessEnvEnum.DATABASE_PORT);
+    const user = appConfigService.getEnvVal(ProcessEnvEnum.DATABASE_USER);
+    const password = appConfigService.getEnvVal(
+      ProcessEnvEnum.DATABASE_PASSWORD,
+    );
+    const databaseName = appConfigService.getEnvVal(
+      ProcessEnvEnum.DATABASE_NAME,
+    );
+
+    const databaseUrl = `postgresql://${user}:${password}@${host}:${port}/${databaseName}?schema=public`;
+
+    super({
+      datasources: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    });
+  }
+
+  async onModuleInit() {}
+  async onModuleDestroy() {}
+}
